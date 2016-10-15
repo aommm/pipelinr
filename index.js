3f4e0532c97e09c5d3905165f489c3fb59803556
Niklas Logren <niklas@niklaslogren.com>
2016-10-15 12:29:58 +0800

    Added own 'curry', which preserves props/toString/applied arguments. Useful for fn serialisation later

--- index.js
+++ index.js
@@ -54,7 +54,8 @@ class Evaluator {
             console.log("vals:", vals);
             // transformation wants array
             if (transformation.manyToOne) {
-                return [transformation(vals)];
+                const nextVal = tryOrUndefined(() => transformation(vals));
+                return [nextVal];
             }
             // 'transformation' is actually many transformations: one per value
             if (_.isArray(transformation)) {
@@ -62,11 +63,12 @@ class Evaluator {
                     throw new Error(`Mismatching number of arguments (${vals.length}) and transformations (${transformation.length})`);
                 }
                 return _.zipWith(transformation, vals, function(transformation, val) {
-                    return transformation(val);
+                    const nextVal = tryOrUndefined(() => transformation(val));
+                    return nextVal;
                 });
             }
             // transformation wants individual values
-            return _.map(vals, transformation);
+            return _.map(vals, (val) => tryOrUndefined(transformation.bind(null, val)));
         }, vals);
 
         if (vals.length !== 1) {
@@ -83,7 +85,73 @@ class HtmlEvaluator extends Evaluator {
     }
     get(selector) {
         const el = this.$(selector);
-        return el && el.text();
+        if (el && el.length) {
+            return el.text();
+        }
+    }
+}
+
+function tryOrUndefined(fn) {
+    try {
+        return fn();
+    } catch (e) {
+        return undefined;
+    }
+}
+
+/**
+ * Sets the toString method of the given function
+ * @param func
+ * @param string
+ */
+function setToString(func, string) {
+    Object.defineProperty(func, 'toString', {
+        'configurable': true,
+        'enumerable': false,
+        'value': _.constant(string),
+        'writable': true
+    });
+};
+
+/**
+ * Creates a curried version of fx, like _.curry
+ * Two important features, for function serialisation:
+ * - Preserves the 'toString' method
+ * - Keeps partially applied arguments in 'appliedArgs' array
+ * @param {Function} fx - function to curry
+ * @returns {Function} - curried function. Has 'appliedArgs' property
+ */
+function curry(fx) {
+    var arity = fx.length;
+    function f1() {
+        var args = Array.prototype.slice.call(arguments, 0);
+        if (args.length >= arity) {
+            return fx.apply(null, args);
+        }
+        else {
+            function f2() {
+                var args2 = Array.prototype.slice.call(arguments, 0);
+                return f1.apply(null, args.concat(args2));
+            }
+            f2.appliedArgs = args; // save arguments
+            copyProps(f1, f2); // if properties are set AFTER curry() call
+            copyProps(fx, f2); // if properties are set BEFORE curry() call
+            setToString(f2, fx + ''); // save original function string
+            return f2;
+        }
+    }
+    f1.appliedArgs = [];
+    copyProps(fx, f1); // copy e.g. 'manyToOne'
+    setToString(f1, fx + ''); // save original function string
+    return f1;
+}
+
+// Copy properties from one object to another
+function copyProps(from, to) {
+    for (var x in from) {
+        if (from.hasOwnProperty(x)) {
+            to[x] = from[x];
+        }
     }
 }
 
@@ -91,19 +159,24 @@ return co(function*() {
     const trim = (x) => x.trim();
     const toLowerCase = (x) => x.toLowerCase();
     const toUpperCase = (x) => x.toUpperCase();
-    const concat = (xs) => _.reduce(xs, (acc, x) => acc + x);
+    const concat = (xs) => xs.join('');
     concat.manyToOne = true;
+    let join = (sep, xs) => xs.join(sep);
+    join.manyToOne = true;
+    join = curry(join);
     const head = (xs) => xs[0];
     head.manyToOne = true;
+    let defaultValue = (def, x) => _.isUndefined(x) ? def : x;
+    defaultValue = curry(defaultValue);
 
     const transformations = [
         '.itemlist .athing:nth-child(1)',
         '.itemlist > tr:nth-child(2) > td:nth-child(2)',
-        [toLowerCase, toUpperCase],
-        //trim,
-        //head,
-        //concat,
-        trim
+        'brokenSelector',
+        [toLowerCase, toUpperCase, toUpperCase],
+        trim,
+        defaultValue("I am a default value"),
+        join('\n')
     ];
 
     const [response, body] = yield request('https://news.ycombinator.com/');
